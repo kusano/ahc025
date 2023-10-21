098372fc7967a2ba39fca47d716137329cc583e1
kusano <kusano@users.noreply.github.com>
2023-10-21 18:49:41 +0900

    ローカルで動かせるようにする
    1617193182
    38,970,645,792
    
    0000.txt  31  2  128  6047551
    0001.txt  56  6 1120  8958531
    0002.txt  94  5 2009 18731329
    0003.txt  80  6 2288 10900989
    0004.txt  43  9  435 17516820
    0005.txt  42 10  202 13051863
    0006.txt  65  2  346  6818801
    0007.txt  79  5  294  8507002
    0008.txt  39  7  197 17795865
    0009.txt  55 12  885 25106006
    0010.txt  65 10 1690 23623706
    0011.txt  59 13  340 17349741
    0012.txt  77 13 1193 20688649
    0013.txt  52 13  954 15551760
    0014.txt  50 11  568 18310074
    0015.txt  44 11  270 14695632

--- A.cpp
+++ A.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <cmath>
+#include <cstdio>
 using namespace std;
 
 int xor64() {
@@ -11,15 +13,114 @@ int xor64() {
     return int(x&0x7fffffff);
 }
 
-int main()
+class Judge
+{
+public:
+    virtual string query(const vector<int> &L, const vector<int> &R) = 0;
+    virtual void answer(const vector<int> ans) = 0;
+};
+
+class Server: public Judge
+{
+public:
+    string query(const vector<int> &L, const vector<int> &R) override
+    {
+        cout<<L.size()<<" "<<R.size();
+        for (int l: L)
+            cout<<" "<<l;
+        for (int r: R)
+            cout<<" "<<r;
+        cout<<endl;
+        string res;
+        cin>>res;
+        return res;
+    }
+
+    void answer(const vector<int> ans) override
+    {
+        for (int i=0; i<(int)ans.size(); i++)
+            cout<<(i==0?"":" ")<<ans[i];
+        cout<<endl;
+    }
+};
+
+class Local: public Judge
+{
+    int N, D, Q;
+    vector<long long> W;
+
+public:
+    Local(int N, int D, int Q, vector<long long> W)
+        : N(N), D(D), Q(Q), W(W)
+    {
+    }
+
+    string query(const vector<int> &L, const vector<int> &R) override
+    {
+        cout<<L.size()<<" "<<R.size();
+        for (int l: L)
+            cout<<" "<<l;
+        for (int r: R)
+            cout<<" "<<r;
+
+        long long wl = 0;
+        for (int l: L)
+            wl += W[l];
+        long long wr = 0;
+        for (int r: R)
+            wr += W[r];
+        if (wl<wr)
+            return "<";
+        else if (wl>wr)
+            return ">";
+        else
+            return "=";
+    }
+
+    void answer(const vector<int> ans) override
+    {
+        for (int i=0; i<(int)ans.size(); i++)
+            cout<<(i==0?"":" ")<<ans[i];
+        cout<<endl;
+
+        vector<long long> T(D);
+        for (int i=0; i<N; i++)
+            T[ans[i]] += W[i];
+
+        double avg = 0;
+        for (long long t: T)
+            avg += t;
+        avg /= D;
+
+        double V = 0;
+        for (long long t: T)
+            V += (t-avg)*(t-avg);
+        V /= D;
+
+        long long score = 1LL + (long long)(100*sqrt(V)+.5);
+        fprintf(stderr, "%3d %2d %4d %8lld\n", N, D, Q, score);
+    }
+};
+
+int main(int argc, char **argv)
 {
     int N, D, Q;
     cin>>N>>D>>Q;
 
+    Judge *judge;
+    if (argc==2 && argv[1]==string("local"))
+    {
+        vector<long long> W(N);
+        for (long long &w: W)
+            cin>>w;
+        judge = new Local(N, D, Q, W);
+    }
+    else
+        judge = new Server();
+
     vector<int> ans;
     for (int i=0; i<N; i++)
         ans.push_back(i%D);
-
 
     int q = 0;
     while (q<Q)
@@ -46,17 +147,8 @@ int main()
             ans[L[xor64()%L.size()]] = r;
         else
         {
-            cout<<L.size()<<" "<<R.size();
-            for (int t: L)
-                cout<<" "<<t;
-            for (int t: R)
-                cout<<" "<<t;
-            cout<<endl;
+            string res = judge->query(L, R);
             q++;
-
-            string res;
-            cin>>res;
-
             if (res=="<")
                 ans[R[xor64()%R.size()]] = l;
             if (res==">")
@@ -64,7 +156,5 @@ int main()
         }
     }
 
-    for (int i=0; i<N; i++)
-        cout<<(i==0?"":" ")<<ans[i];
-    cout<<endl;
+    judge->answer(ans);
 }
